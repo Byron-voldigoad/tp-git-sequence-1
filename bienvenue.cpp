4d3bd8078f9ac84d99973e1a2b290e1538ef62b7
Byron-voldigoad <tchangolouismullergmail.com>
2026-02-06 13:19:59 +0100

    Ajout du fichier source bienvenue.cpp

---
+++ bienvenue.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    // Afficher un message de bienvenue
+    std::cout << "Bienvenue dans le TP de gestion de configurations !" << std::endl;
+    
+    return 0;
+}
